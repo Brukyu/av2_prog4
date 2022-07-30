2577eca363b5bef7fe12b434f1583ffe4468191d
Bruno Igor <bbrunoigorr@gmail.com>
2022-07-29 22:07:01 -0300

    hangman

---
+++ lib/app.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'screens/home.dart';
+import 'screens/result.dart';
+
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "Jogo da Forca",
+      theme: ThemeData(primarySwatch: Colors.red),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const HomePage(),
+        '/result': (context) => const ResultPage()
+      },
+    );
+  }
+}
